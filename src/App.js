73bd34ddbb70cf7b00b028386b1a8a00c4453d36
Omikhe <msakamike84@gmail.com>
2024-05-02 10:51:06 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex  flex-row w-full h-screen items-center justify-center bg-yellow-600">
+      My APP
+    </div>
+  );
+}
+
+export default App;
